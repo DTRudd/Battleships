7cb904848d65c9f2951403bff281a39f125656db
Sophie Rudd <DTRudd@outlook.com>
2017-04-28 16:43:21 +0100

    Stage 2.  Two ships validated and placed each.

--- Battleships.java
+++ Battleships.java
@@ -5,8 +5,15 @@ public class Battleships {
 	public static void main(String[] Args){
 		Board player1Board = new Board(3);
 		Board player2Board = new Board(3);
-		player1Board.getCoOrds(1, 2);
-		player2Board.getCoOrds(2, 1);
+		TestShip p1Ship = new TestShip();
+		TestShip p2Ship = new TestShip();
+		try{
+			p1Ship.place(player1Board, 0, 0, Ship.Orientation.UP);
+			p2Ship.place(player2Board, 2, 1, Ship.Orientation.LEFT);
+		} catch (improperPlacementException ipe){
+			System.out.println(ipe.getMessage());
+			ipe.printStackTrace();
+		}
 	}
 
 }
