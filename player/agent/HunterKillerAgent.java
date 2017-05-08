05fd3618fac4348db38f57cf5af6e5ffcb7befc4
Sophie Rudd <DTRudd@outlook.com>
2017-05-08 10:31:57 +0100

    Revert "Revert "Fully working terminal-based 0-player game""
    This reverts commit 9db86733fd8a0d049fc1b65c27a7d3388d1e9e09.

--- player/agent/HunterKillerAgent.java
+++ player/agent/HunterKillerAgent.java
@@ -67,7 +67,10 @@ public class HunterKillerAgent extends Agent {
 	@Override
 	public Tuple<Integer, Integer> getAttackVector(Game g, Scanner sc) {
 		if (enemyFleet == null){
-			enemyFleet = findOpponent().getFleet();
+			enemyFleet = new ArrayList<Ship>();
+			for (Ship s : findOpponent().getFleet()){
+				enemyFleet.add(s);
+			}
 		}
 		int tmp = enemyFleet.stream().map(Ship::getLength).reduce(Math::min).get();
 		if (tmp != paritySize){
